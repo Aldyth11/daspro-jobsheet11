7a93a9df5ba93668d58e776637902de7ec05ae89
Aldyth11 <aldyth.r.f@gmail.com>
2024-11-21 10:24:01 +0700

    Percobaan 2 jawaban 5

--- Kafe14.java
+++ Kafe14.java
@@ -1,5 +1,5 @@
 public class Kafe14 {
-    public static void Menu(String namaPelanggan, boolean isMember) {
+    public static void Menu(String namaPelanggan, boolean isMember, String kodePromo) {
         System.out.println("Selamat datang, " + namaPelanggan + "!");
         if (isMember) {
             System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
@@ -14,8 +14,17 @@ public class Kafe14 {
         System.out.println("6. Mie Goreng - Rp 18,000");
         System.out.println("===========================");
         System.out.println("Silahkan pilih menu yang Anda inginkan.");
+
+        if (kodePromo.equals("DISKON50")) {
+            System.out.println("Selamat! Anda mendapatkan diskon 50%.");
+        } else if (kodePromo.equals("DISKON30")) {
+            System.out.println("Selamat! Anda mendapatkan diskon 30%.");
+        } else {
+            System.out.println("Kode promo tidak valid.");
+        }
     }
     public static void main(String[] args) {
-        Menu("Andi", true);
+        Menu("Andi", true, "DISKON50");
+        Menu("Rafi", true, "DISKON30");
     }
 }
