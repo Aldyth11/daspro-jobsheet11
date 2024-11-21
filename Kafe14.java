799f6da96fb53018dc2809d0d747fef3f9898e9e
Aldyth11 <aldyth.r.f@gmail.com>
2024-11-21 10:17:01 +0700

    Percobaan 2

--- Kafe14.java
+++ Kafe14.java
@@ -1,5 +1,10 @@
 public class Kafe14 {
-    public static void Menu() {
+    public static void Menu(String namaPelanggan, boolean isMember) {
+        System.out.println("Selamat datang, " + namaPelanggan + "!");
+        if (isMember) {
+            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+        }
+
         System.out.println("===== MENU RESTO CAFE =====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Cappuccino - Rp 20,000");
@@ -11,6 +16,6 @@ public class Kafe14 {
         System.out.println("Silahkan pilih menu yang Anda inginkan.");
     }
     public static void main(String[] args) {
-        Menu ();
+        Menu("Andi", true);
     }
 }
